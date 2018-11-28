5f80d17d588151f18d75a6f30661feaf99301fea
Michelle Vann <michelle@dougvann.com>
2018-11-28 15:08:26 -0500

    Database and Connection String

---
+++ HomePlanIt.Data/DIY.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomePlanIt.Data
+{
+    public class DIY
+    {
+        [Key]
+        public int ProjectId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string ProjectName { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public decimal TotalCost { get; set; }
+
+        public bool IsFinished { get; set; }
+    }
+}
